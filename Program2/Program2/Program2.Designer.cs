4a8416d7934d25b260e0a7ba0ce1d54f2dcf0447
ngoodridge <47260902+ngoodridge@users.noreply.github.com>
2019-04-13 16:58:05 -0400

    Initial commit

---
+++ Program2/Program2/Program2.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Program2
+{
+    partial class Program2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NamePromptLabel = new System.Windows.Forms.Label();
+            this.LastInitialBox = new System.Windows.Forms.TextBox();
+            this.ClassPromptLabel = new System.Windows.Forms.Label();
+            this.FreshmanButton = new System.Windows.Forms.RadioButton();
+            this.SophmoreButton = new System.Windows.Forms.RadioButton();
+            this.JuniorButton = new System.Windows.Forms.RadioButton();
+            this.SeniorButton = new System.Windows.Forms.RadioButton();
+            this.DateLabel = new System.Windows.Forms.Label();
+            this.DateOutputLabel = new System.Windows.Forms.Label();
+            this.TimeLabel = new System.Windows.Forms.Label();
+            this.TimeOutputLabel = new System.Windows.Forms.Label();
+            this.RegistrationButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NamePromptLabel
+            // 
+            this.NamePromptLabel.AutoSize = true;
+            this.NamePromptLabel.Location = new System.Drawing.Point(12, 18);
+            this.NamePromptLabel.Name = "NamePromptLabel";
+            this.NamePromptLabel.Size = new System.Drawing.Size(215, 13);
+            this.NamePromptLabel.TabIndex = 0;
+            this.NamePromptLabel.Text = "Please enter the first letter of your last name:";
+            // 
+            // LastInitialBox
+            // 
+            this.LastInitialBox.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.LastInitialBox.Location = new System.Drawing.Point(233, 15);
+            this.LastInitialBox.MaxLength = 1;
+            this.LastInitialBox.Name = "LastInitialBox";
+            this.LastInitialBox.Size = new System.Drawing.Size(22, 20);
+            this.LastInitialBox.TabIndex = 1;
+            // 
+            // ClassPromptLabel
+            // 
+            this.ClassPromptLabel.AutoSize = true;
+            this.ClassPromptLabel.Location = new System.Drawing.Point(12, 56);
+            this.ClassPromptLabel.Name = "ClassPromptLabel";
+            this.ClassPromptLabel.Size = new System.Drawing.Size(230, 13);
+            this.ClassPromptLabel.TabIndex = 2;
+            this.ClassPromptLabel.Text = "Please select you current class standing below:";
+            // 
+            // FreshmanButton
+            // 
+            this.FreshmanButton.AutoSize = true;
+            this.FreshmanButton.Location = new System.Drawing.Point(15, 72);
+            this.FreshmanButton.Name = "FreshmanButton";
+            this.FreshmanButton.Size = new System.Drawing.Size(71, 17);
+            this.FreshmanButton.TabIndex = 3;
+            this.FreshmanButton.TabStop = true;
+            this.FreshmanButton.Text = "Freshman";
+            this.FreshmanButton.UseVisualStyleBackColor = true;
+            // 
+            // SophmoreButton
+            // 
+            this.SophmoreButton.AutoSize = true;
+            this.SophmoreButton.Location = new System.Drawing.Point(142, 72);
+            this.SophmoreButton.Name = "SophmoreButton";
+            this.SophmoreButton.Size = new System.Drawing.Size(73, 17);
+            this.SophmoreButton.TabIndex = 4;
+            this.SophmoreButton.TabStop = true;
+            this.SophmoreButton.Text = "Sophmore";
+            this.SophmoreButton.UseVisualStyleBackColor = true;
+            // 
+            // JuniorButton
+            // 
+            this.JuniorButton.AutoSize = true;
+            this.JuniorButton.Location = new System.Drawing.Point(15, 95);
+            this.JuniorButton.Name = "JuniorButton";
+            this.JuniorButton.Size = new System.Drawing.Size(53, 17);
+            this.JuniorButton.TabIndex = 5;
+            this.JuniorButton.TabStop = true;
+            this.JuniorButton.Text = "Junior";
+            this.JuniorButton.UseVisualStyleBackColor = true;
+            // 
+            // SeniorButton
+            // 
+            this.SeniorButton.AutoSize = true;
+            this.SeniorButton.Location = new System.Drawing.Point(142, 95);
+            this.SeniorButton.Name = "SeniorButton";
+            this.SeniorButton.Size = new System.Drawing.Size(55, 17);
+            this.SeniorButton.TabIndex = 6;
+            this.SeniorButton.TabStop = true;
+            this.SeniorButton.Text = "Senior";
+            this.SeniorButton.UseVisualStyleBackColor = true;
+            // 
+            // DateLabel
+            // 
+            this.DateLabel.AutoSize = true;
+            this.DateLabel.Location = new System.Drawing.Point(12, 128);
+            this.DateLabel.Name = "DateLabel";
+            this.DateLabel.Size = new System.Drawing.Size(188, 13);
+            this.DateLabel.TabIndex = 7;
+            this.DateLabel.Text = "You can register on the following date:";
+            // 
+            // DateOutputLabel
+            // 
+            this.DateOutputLabel.AutoSize = true;
+            this.DateOutputLabel.Location = new System.Drawing.Point(206, 128);
+            this.DateOutputLabel.Name = "DateOutputLabel";
+            this.DateOutputLabel.Size = new System.Drawing.Size(0, 13);
+            this.DateOutputLabel.TabIndex = 8;
+            // 
+            // TimeLabel
+            // 
+            this.TimeLabel.AutoSize = true;
+            this.TimeLabel.Location = new System.Drawing.Point(12, 155);
+            this.TimeLabel.Name = "TimeLabel";
+            this.TimeLabel.Size = new System.Drawing.Size(183, 13);
+            this.TimeLabel.TabIndex = 9;
+            this.TimeLabel.Text = "You can register at the following time:";
+            // 
+            // TimeOutputLabel
+            // 
+            this.TimeOutputLabel.AutoSize = true;
+            this.TimeOutputLabel.Location = new System.Drawing.Point(206, 155);
+            this.TimeOutputLabel.Name = "TimeOutputLabel";
+            this.TimeOutputLabel.Size = new System.Drawing.Size(0, 13);
+            this.TimeOutputLabel.TabIndex = 10;
+            // 
+            // RegistrationButton
+            // 
+            this.RegistrationButton.Location = new System.Drawing.Point(85, 199);
+            this.RegistrationButton.Name = "RegistrationButton";
+            this.RegistrationButton.Size = new System.Drawing.Size(110, 21);
+            this.RegistrationButton.TabIndex = 11;
+            this.RegistrationButton.Text = "When do I register?";
+            this.RegistrationButton.UseVisualStyleBackColor = true;
+            this.RegistrationButton.Click += new System.EventHandler(this.RegistrationButton_Click);
+            // 
+            // Program2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.RegistrationButton);
+            this.Controls.Add(this.TimeOutputLabel);
+            this.Controls.Add(this.TimeLabel);
+            this.Controls.Add(this.DateOutputLabel);
+            this.Controls.Add(this.DateLabel);
+            this.Controls.Add(this.SeniorButton);
+            this.Controls.Add(this.JuniorButton);
+            this.Controls.Add(this.SophmoreButton);
+            this.Controls.Add(this.FreshmanButton);
+            this.Controls.Add(this.ClassPromptLabel);
+            this.Controls.Add(this.LastInitialBox);
+            this.Controls.Add(this.NamePromptLabel);
+            this.Name = "Program2";
+            this.Text = "Program 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label NamePromptLabel;
+        private System.Windows.Forms.TextBox LastInitialBox;
+        private System.Windows.Forms.Label ClassPromptLabel;
+        private System.Windows.Forms.RadioButton FreshmanButton;
+        private System.Windows.Forms.RadioButton SophmoreButton;
+        private System.Windows.Forms.RadioButton JuniorButton;
+        private System.Windows.Forms.RadioButton SeniorButton;
+        private System.Windows.Forms.Label DateLabel;
+        private System.Windows.Forms.Label DateOutputLabel;
+        private System.Windows.Forms.Label TimeLabel;
+        private System.Windows.Forms.Label TimeOutputLabel;
+        private System.Windows.Forms.Button RegistrationButton;
+    }
+}
+
---
+++ Program2/Program2/Program2.cs
@@ -0,0 +1,220 @@
+﻿/*Grading ID: A7272
+ * Class: CIS 199-01
+ * Program 2
+ * Due: 10/19/17
+ * Description: This program accepts a letter input from the user and a radio button selection to determine when the student is able to register for classes*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class Program2 : Form
+    {
+        public Program2()
+        {
+            InitializeComponent();
+        }
+
+        private void RegistrationButton_Click(object sender, EventArgs e)
+        {
+            char LastInitial = char.ToUpper(char.Parse(LastInitialBox.Text));  //Reads the Char the user entered, converts it to upper-case and stores it in the variable
+
+            //This is the section for the freshman
+            if (FreshmanButton.Checked == true && (LastInitial == 'A' || LastInitial == 'B')) //Checks that the freshman button was selcted and what letter was entered
+            {
+                DateOutputLabel.Text = "Nov. 9th";//outputs the registration date into the label
+                TimeOutputLabel.Text = "11:30am";//outputs the registration time into the label
+            }
+
+            else if (FreshmanButton.Checked == true && (LastInitial == 'C' || LastInitial == 'D'))
+            {
+                DateOutputLabel.Text = "Nov. 9th";
+                TimeOutputLabel.Text = "2:00pm";
+            }
+
+            else if (FreshmanButton.Checked == true && (LastInitial == 'E' || LastInitial == 'F'))
+            {
+                DateOutputLabel.Text = "Nov. 9th";
+                TimeOutputLabel.Text = "4:00pm";
+            }
+
+            else if (FreshmanButton.Checked == true && (LastInitial == 'G' || LastInitial == 'H' || LastInitial == 'I'))
+            {
+                DateOutputLabel.Text = "Nov. 10th";
+                TimeOutputLabel.Text = "8:30am";
+            }
+
+            else if (FreshmanButton.Checked == true && (LastInitial == 'J' || LastInitial == 'K' || LastInitial == 'L'))
+            {
+                DateOutputLabel.Text = "Nov. 10th";
+                TimeOutputLabel.Text = "10:00am";
+            }
+
+            else if (FreshmanButton.Checked == true && (LastInitial == 'M' || LastInitial == 'N' || LastInitial == 'O'))
+            {
+                DateOutputLabel.Text = "Nov. 10th";
+                TimeOutputLabel.Text = "11:30am";
+            }
+
+            else if (FreshmanButton.Checked == true && (LastInitial == 'P' || LastInitial == 'Q'))
+            {
+                DateOutputLabel.Text = "Nov. 10th";
+                TimeOutputLabel.Text = "2:00pm";
+            }
+
+            else if (FreshmanButton.Checked == true && (LastInitial == 'R' || LastInitial == 'S'))
+            {
+                DateOutputLabel.Text = "Nov. 10th";
+                TimeOutputLabel.Text = "4:00pm";
+            }
+
+            else if (FreshmanButton.Checked == true && (LastInitial == 'T' || LastInitial == 'U' || LastInitial == 'V'))
+            {
+                DateOutputLabel.Text = "Nov. 9th";
+                TimeOutputLabel.Text = "8:30am";
+            }
+
+            else if (FreshmanButton.Checked == true && (LastInitial == 'W' || LastInitial == 'X' || LastInitial == 'Y' || LastInitial == 'Z'))
+            {
+                DateOutputLabel.Text = "Nov. 9th";
+                TimeOutputLabel.Text = "10:00am";
+            }
+
+            //This section is for the Sophmores
+
+            else if (SophmoreButton.Checked == true && (LastInitial == 'A' || LastInitial == 'B'))
+            {
+                DateOutputLabel.Text = "Nov. 7th";
+                TimeOutputLabel.Text = "11:30am";
+            }
+
+            else if (SophmoreButton.Checked == true && (LastInitial == 'C' || LastInitial == 'D'))
+            {
+                DateOutputLabel.Text = "Nov. 7th";
+                TimeOutputLabel.Text = "2:00pm";
+            }
+
+            else if (SophmoreButton.Checked == true && (LastInitial == 'E' || LastInitial == 'F'))
+            {
+                DateOutputLabel.Text = "Nov. 7th";
+                TimeOutputLabel.Text = "4:00pm";
+            }
+
+            else if (SophmoreButton.Checked == true && (LastInitial == 'G' || LastInitial == 'H' || LastInitial == 'I'))
+            {
+                DateOutputLabel.Text = "Nov. 8th";
+                TimeOutputLabel.Text = "8:30am";
+            }
+
+            else if (SophmoreButton.Checked == true && (LastInitial == 'J' || LastInitial == 'K' || LastInitial == 'L'))
+            {
+                DateOutputLabel.Text = "Nov. 8th";
+                TimeOutputLabel.Text = "10:00am";
+            }
+
+            else if (SophmoreButton.Checked == true && (LastInitial == 'M' || LastInitial == 'N' || LastInitial == 'O'))
+            {
+                DateOutputLabel.Text = "Nov. 8th";
+                TimeOutputLabel.Text = "11:30am";
+            }
+
+            else if (SophmoreButton.Checked == true && (LastInitial == 'P' || LastInitial == 'Q'))
+            {
+                DateOutputLabel.Text = "Nov. 8th";
+                TimeOutputLabel.Text = "2:00pm";
+            }
+
+            else if (SophmoreButton.Checked == true && (LastInitial == 'R' || LastInitial == 'S'))
+            {
+                DateOutputLabel.Text = "Nov. 8th";
+                TimeOutputLabel.Text = "4:00pm";
+            }
+
+            else if (SophmoreButton.Checked == true && (LastInitial == 'T' || LastInitial == 'U' || LastInitial == 'V'))
+            {
+                DateOutputLabel.Text = "Nov. 7th";
+                TimeOutputLabel.Text = "8:30am";
+            }
+
+            else if (SophmoreButton.Checked == true && (LastInitial == 'W' || LastInitial == 'X' || LastInitial == 'Y' || LastInitial == 'Z'))
+            {
+                DateOutputLabel.Text = "Nov. 7th";
+                TimeOutputLabel.Text = "10:00am";
+            }
+
+            //This section is for Juniors
+
+            else if (JuniorButton.Checked == true && (LastInitial == 'A' || LastInitial == 'B' || LastInitial == 'C' || LastInitial == 'D'))
+            {
+                DateOutputLabel.Text = "Nov. 6th";
+                TimeOutputLabel.Text = "10:00am";
+            }
+
+            else if (JuniorButton.Checked == true && (LastInitial == 'E' || LastInitial == 'F' || LastInitial == 'G' || LastInitial == 'H' || LastInitial == 'I'))
+            {
+                DateOutputLabel.Text = "Nov. 6th";
+                TimeOutputLabel.Text = "11:30am";
+            }
+
+            else if (JuniorButton.Checked == true && (LastInitial == 'J' || LastInitial == 'K' || LastInitial == 'L' || LastInitial == 'M' || LastInitial == 'M' || LastInitial == 'O'))
+            {
+                DateOutputLabel.Text = "Nov. 6th";
+                TimeOutputLabel.Text = "2:00pm";
+            }
+
+            else if (JuniorButton.Checked == true && (LastInitial == 'P' || LastInitial == 'Q' || LastInitial == 'R' || LastInitial == 'S'))
+            {
+                DateOutputLabel.Text = "Nov. 6th";
+                TimeOutputLabel.Text = "4:00pm";
+            }
+
+            else if (JuniorButton.Checked == true && (LastInitial == 'T' || LastInitial == 'U' || LastInitial == 'V' || LastInitial == 'W' || LastInitial == 'X' || LastInitial == 'Y' || LastInitial == 'Z'))
+            {
+                DateOutputLabel.Text = "Nov. 6th";
+                TimeOutputLabel.Text = "8:30am";
+            }
+
+            //This section is for Seniors
+
+            else if (SeniorButton.Checked == true && (LastInitial == 'A' || LastInitial == 'B' || LastInitial == 'C' || LastInitial == 'D'))
+            {
+                DateOutputLabel.Text = "Nov. 3rd";
+                TimeOutputLabel.Text = "10:00am";
+            }
+
+            else if (SeniorButton.Checked == true && (LastInitial == 'E' || LastInitial == 'F' || LastInitial == 'G' || LastInitial == 'H' || LastInitial == 'I'))
+            {
+                DateOutputLabel.Text = "Nov. 3rd";
+                TimeOutputLabel.Text = "11:30am";
+            }
+
+            else if (SeniorButton.Checked == true && (LastInitial == 'J' || LastInitial == 'K' || LastInitial == 'L' || LastInitial == 'M' || LastInitial == 'M' || LastInitial == 'O'))
+            {
+                DateOutputLabel.Text = "Nov. 3rd";
+                TimeOutputLabel.Text = "2:00pm";
+            }
+
+            else if (SeniorButton.Checked == true && (LastInitial == 'P' || LastInitial == 'Q' || LastInitial == 'R' || LastInitial == 'S'))
+            {
+                DateOutputLabel.Text = "Nov. 3rd";
+                TimeOutputLabel.Text = "4:00pm";
+            }
+
+            else if (SeniorButton.Checked == true && (LastInitial == 'T' || LastInitial == 'U' || LastInitial == 'V' || LastInitial == 'W' || LastInitial == 'X' || LastInitial == 'Y' || LastInitial == 'Z'))
+            {
+                DateOutputLabel.Text = "Nov. 3rd";
+                TimeOutputLabel.Text = "8:30am";
+            }
+
+
+        }
+    }
+}
